220c396a8122f1eca61a8f456b940fbae0370e4d
dvkt <c@dvkt.io>
2019-04-04 18:18:22 -0700

    add load/save history subprocedures

--- ldplnoise.cpp
+++ ldplnoise.cpp
@@ -2,7 +2,18 @@
 #include "ldplnoise.h"
 #include "linenoise.hpp"
 
-void RL_ACCEPT() {
+void RL_LOAD_HISTORY() 
+{
+    linenoise::LoadHistory(RL_HISTORY_FILE.c_str());
+}
+
+void RL_SAVE_HISTORY()
+{
+    linenoise::SaveHistory(RL_HISTORY_FILE.c_str());
+}
+
+void RL_ACCEPT() 
+{
     linenoise::SetHistoryMaxLen(100);
     linenoise::SetMultiLine(true);
 
--- ldplnoise.h
+++ ldplnoise.h
@@ -2,11 +2,17 @@
 
 using std::string;
 
-extern string VAR_RLr_UINPUT;  // RL-INPUT
-extern string VAR_RLr_UPROMPT; // RL-PROMPT
-void SUBPR_RLr_UACCEPT();      // RL-ACCEPT
+extern string VAR_RLr_UINPUT;           // RL-INPUT
+extern string VAR_RLr_UPROMPT;          // RL-PROMPT
+extern string VAR_RLr_UHISTORYr_UFILE;  // RL-HISTORY-FILE
+void SUBPR_RLr_UACCEPT();               // RL-ACCEPT
+void SUBPR_RLr_USAVEr_UHISTORY();       // RL-SAVE-HISTORY
+void SUBPR_RLr_ULOADr_UHISTORY();       // RL-LOAD-HISTORY
 
-#define RL_INPUT  VAR_RLr_UINPUT
-#define RL_PROMPT VAR_RLr_UPROMPT
-#define RL_ACCEPT SUBPR_RLr_UACCEPT
+#define RL_HISTORY_FILE VAR_RLr_UHISTORYr_UFILE
+#define RL_INPUT        VAR_RLr_UINPUT
+#define RL_PROMPT       VAR_RLr_UPROMPT
+#define RL_ACCEPT       SUBPR_RLr_UACCEPT
+#define RL_SAVE_HISTORY SUBPR_RLr_USAVEr_UHISTORY
+#define RL_LOAD_HISTORY SUBPR_RLr_ULOADr_UHISTORY
 
