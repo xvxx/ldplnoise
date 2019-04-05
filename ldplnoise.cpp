7df5d1b9c2c07c0187c9ba2780195c45cbc59293
dvkt <c@dvkt.io>
2019-04-04 20:00:55 -0700

    RL-ADD-HISTORY

--- ldplnoise.cpp
+++ ldplnoise.cpp
@@ -12,11 +12,13 @@ void RL_SAVE_HISTORY()
     linenoise::SaveHistory(RL_HISTORY_FILE.c_str());
 }
 
+void RL_ADD_HISTORY()
+{
+    linenoise::AddHistory(RL_INPUT.c_str());
+}
+
 void RL_ACCEPT() 
 {
     linenoise::SetHistoryMaxLen(100);
-    linenoise::SetMultiLine(true);
-
     linenoise::Readline(RL_PROMPT.c_str(), RL_INPUT);
-    linenoise::AddHistory(RL_INPUT.c_str());
 }
