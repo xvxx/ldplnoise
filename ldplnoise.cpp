dff8736f2ea02743f982f3703055f591483a7eb4
dvkt <c@dvkt.io>
2019-04-08 18:03:06 -0700

    external variables

--- ldplnoise.cpp
+++ ldplnoise.cpp
@@ -4,17 +4,9 @@
 
 using std::string;
 
-extern string VAR_RLr_UINPUT;           // RL-INPUT
-extern string VAR_RLr_UPROMPT;          // RL-PROMPT
-extern string VAR_RLr_UHISTORYr_UFILE;  // RL-HISTORY-FILE
-
-#define RL_HISTORY_FILE VAR_RLr_UHISTORYr_UFILE
-#define RL_INPUT        VAR_RLr_UINPUT
-#define RL_PROMPT       VAR_RLr_UPROMPT
-#define RL_ACCEPT       SUBPR_RLr_UACCEPT
-#define RL_SAVE_HISTORY SUBPR_RLr_USAVEr_UHISTORY
-#define RL_LOAD_HISTORY SUBPR_RLr_ULOADr_UHISTORY
-#define RL_ADD_HISTORY  SUBPR_RLr_UADDr_UHISTORY
+string RL_INPUT;         // RL-INPUT
+string RL_PROMPT;        // RL-PROMPT
+string RL_HISTORY_FILE;  // RL-HISTORY-FILE
 
 void RL_LOAD_HISTORY() 
 {
